de011b634bfd1c72095a7b766fa6054298c875a4
PhmNm <19120597@student.hcmus.edu.vn>
2022-04-24 10:23:48 +0700

    complete

--- crm1/settings.py
+++ crm1/settings.py
@@ -137,14 +137,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# with open('./crm1/email.txt','r') as email:
-#     name = email.readline().strip()
-#     password = email.readline().strip()
-#SMTP Configuration
+with open('./crm1/email.txt','r') as email:
+    name = email.readline().strip()
+    password = email.readline().strip()
+
+# SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'phamvannam12344@gmail.com'
-EMAIL_HOST_PASSWORD = 'Truong123456'
+EMAIL_HOST_USER = name
+EMAIL_HOST_PASSWORD = password
