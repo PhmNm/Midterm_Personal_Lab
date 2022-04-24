26647cb4cff77c500e9d9dfb6f4b85250b5755dd
PhmNm <19120597@student.hcmus.edu.vn>
2022-04-24 10:11:24 +0700

    nothing-is-last

--- crm1/settings.py
+++ crm1/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhitenoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
